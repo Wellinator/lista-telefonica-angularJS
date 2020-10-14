c74f409f7e224c847bf304510dc2dafe2af7b95b
wellcoj <wjunior@brasiliano.com.br>
2020-10-14 17:51:58 -0300

    Modulariza alert directive

--- src/app/js/directives/uiAlertDirective.js
+++ src/app/js/directives/uiAlertDirective.js
@@ -1,5 +1,12 @@
 angular.module("listaTelefonica").directive("uiAlert", function () {
     return {
-        templateUrl: "view/alert.html", 
+        templateUrl: "view/alert.html",
+        replace: true,
+        restrict: "AE",
+        scope: {
+            title: "@",
+            message: "="
+        },
+        transclude: true
     }
 })
